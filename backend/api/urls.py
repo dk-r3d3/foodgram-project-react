28878a9dad3c970619cd4e750027c572ba6c6bdb
Дмитрий Коротеев <blessdevil@yandex.ru>
2022-09-04 14:47:37 +0700

    fixed BD, frontend, backend

---
+++ backend/api/urls.py
@@ -0,0 +1,22 @@
+from rest_framework import routers
+from django.urls import include, path, re_path
+
+from recipes.views import (
+    IngredientsViewSet, TagViewSet, RecipesViewSet
+)
+from users.views import UserViewSet
+
+app_name = 'api'
+
+router = routers.DefaultRouter()
+
+router.register(r'users', UserViewSet, basename='users')
+router.register(r'ingredients', IngredientsViewSet, basename='ingredients')
+router.register(r'tags', TagViewSet, basename='tags')
+router.register(r'recipes', RecipesViewSet, basename='recipes')
+
+urlpatterns = [
+    path('', include('djoser.urls')),
+    path('', include(router.urls)),
+    re_path(r'auth/', include('djoser.urls.authtoken')),
+]
---
+++ backend/foodgram/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+api_paths = [
+    path('', include('api.urls')),
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+    urlpatterns += static(
+        settings.STATIC_URL, document_root=settings.STATIC_ROOT
+    )
